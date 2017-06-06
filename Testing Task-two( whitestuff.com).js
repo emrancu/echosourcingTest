eb854f504f58844ef38a609e0bfe0a52209388eb
M A EMRAN <M A EMRAN>
2017-06-06 15:16:48 +0600

    Change size filtering box

--- Testing Task-two( whitestuff.com).js
+++ Testing Task-two( whitestuff.com).js
@@ -22,36 +22,48 @@ var headerCSS={'background-image':'none', 'background-color': '#717B87','color':
 var listCSS={'display':'inline-block','width':'33.3333%'};
 
 
-// change box header text
+// select  size filtering stiky box
+var sizeArea=$("#facetMiddleBlock>div.bd-color2:first-child");
+
+// assign container css of size filtering stiky box
+sizeArea.css(fixerCSS);
+
+
+// select  box header area
 var headerarea=$("#facetMiddleBlock>div.bd-color2:first-child div.header");
 
+// change box header text
 headerarea.text("Shop Your Size");
 
 
+// assign css for size filtering box header
+$("#facetMiddleBlock>div.bd-color2:first-child div.header").css(headerCSS);
+
+
+// declare   css for minize and maximize button  
 var btncsstext="float: right; position: absolute;top: 0px;right: 0px;height: 28px;display: block;background: #616A75;width: 33px;text-align: center;padding-top: 5px";
-	
 
-
-headerarea.next('ul').prepend('<p style="padding: 10px">select your size to see item that are  in stock for you</p>');
-
+// declare svg icon content for minize button
 var minimizeBTN='<svg fill="white" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M19 13H5v-2h14v2z"/> <path d="M0 0h24v24H0z" fill="none"/> </svg>';
 
+// declare svg icon content for maximize button
 var morebtn='<svg fill="white" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
-
+	
+// set maximize button as default
 headerarea.append('<span style="'+btncsstext+'">'+morebtn+"</span>");
 
-// assign container css of size filtering stiky box
-$("#facetMiddleBlock>div.bd-color2:first-child").css(fixerCSS);
 
-// assign css of size filtering box header
+// add text after box header
+headerarea.next('ul').prepend('<p style="padding: 10px">select your size to see item that are  in stock for you</p>');
 
-$("#facetMiddleBlock>div.bd-color2:first-child div.header").css(headerCSS);
+
 
 // assign css of size filtering list
 $("#facetMiddleBlock>div.bd-color2:first-child ul>li").css(listCSS);
  
+
  
- 
+ // change minimize and maximize button depend on click event 
  headerarea.click(function(){
 	if($(this).hasClass('opened')){
 	$(this).find('span').html(morebtn);	
@@ -63,9 +75,9 @@ $("#facetMiddleBlock>div.bd-color2:first-child ul>li").css(listCSS);
  
  
  
-  
-headerarea.next('ul>li').click(function(){
-	headerarea.next('ul>p').text('Select several sizes to broaiden your chooice' ) ;
+ // change after header  text depend on select 
+sizeArea.find('ul>li>a').click(function(){
+	sizeArea.find('ul>p').text('Select several sizes to broaiden your chooice' ) ;
 	 
  });
  
